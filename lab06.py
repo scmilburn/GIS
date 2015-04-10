2b809adaf945a49232ada26694f425cb729d5c24
Simon <simoncmilburn@gmail.com>
2015-04-09 21:12:24 -0400

    l6 update

--- lab06.py
+++ lab06.py
@@ -6,36 +6,51 @@
 # 4/2/2015        #
  #################
 
-# The goal of this lab is to develop skills  to create and modify geoetries of geospatial data
+# The goal of this lab is to develop skills to create and modify geometries of geospatial data
 
 import arcpy
 
-source = "R:/376/Spring15/Lab06/lab06_data/"
+print "imported"
+
+#Setting up path variables
 path = "C:/Users/smilburn/Desktop/lab06_data/"
 
-size = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-feature_info = []
-
-for x in size:
-	for y in size:
-		feature_info.append[x,y]
+#range of northern hemisphere
+lat = range(0,90,10)
+lon = range(-180,180,10)
 
 grid = []
-for feature in feature_info:
-	grid.append(
-		arcpy.Polygon(
-			arcpy.Array([arcpy.Point(*coords) for coords in feature])))
 
-sp = arcpy.Describe(path + "world.shp").spatialReference
-gcs = sp.GCSName
+#creating polygon list
+for y in lat:
+    for x in lon:
+        arr = []
+
+        p1 = arcpy.Point(x, y)
+        p2 = arcpy.Point(x+10, y)
+        p3 = arcpy.Point(x, y+10)
+        p4 = arcpy.Point(x+10, y+10)
+
+        arr.append(p3)
+        arr.append(p4)
+        arr.append(p2)
+        arr.append(p1)
+        
+        grid.append(arcpy.Polygon(arcpy.Array(arr)))
+print "done for"
+
+#making polylist into .shp
+arcpy.CopyFeatures_management(grid, path + "grid.shp")
+print "made grid.shp"
 
 #DEFINING PROJECTION BASED OFF OF world.shp
-arcpy.DefineProjection_management(grid, gcs)
+arcpy.DefineProjection_management(path + "grid.shp", 4326)
 
-#ADDING UID Field
-arcpy.AddField_management(grid, "UID", int)
+#ADDING UID Field and populating with unique ids 
+arcpy.AddField_management(path + "grid.shp", "UID", "LONG")
+arcpy.CalculateField_management(path + "grid.shp", "UID", "!FID! + 1", "PYTHON")
 
-#Populating field with unique identifiers 
-
-
-
+#Intersecting shapes and getting areas
+arcpy.Intersect_analysis([path + "grid.shp", path + "world.shp"], path + "intersect.shp")
+arcpy.Dissolve_management()
+arcpy.CalculateAreas_stats(path + "dissolved.shp", path + "dissolved_farea.shp")
